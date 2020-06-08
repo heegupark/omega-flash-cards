911861451dd8d2a5e97e76694030fcb5295f24e2
heegupark <slhead@gmail.com>
2020-06-08 13:04:07 -0700

    Changed css

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,16 +8,16 @@ class Nav extends Component {
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse nav-group-custom" id="navbarNav">
+          <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item nav-item-custom">
-                <a className={this.props.active === 'view-cards' ? "nav-link nav-link-custom" : "nav-link"} href="#" id="view-cards" onClick={this.props.setView}>My Cards</a>
+              <li className="nav-item ml-2 mr-2">
+                <a className={this.props.active === 'view-cards' ? "nav-link btn btn-primary" : "nav-link btn"} href="#" id="view-cards" onClick={this.props.setView}>My Cards</a>
               </li>
-              <li className="nav-item nav-item-custom">
-                <a className={this.props.active === 'review-cards' ? "nav-link nav-link-custom" : "nav-link"} href="#" id="review-cards" onClick={this.props.setView}>Review Cards</a>
+              <li className="nav-item ml-2 mr-2">
+                <a className={this.props.active === 'review-cards' ? "nav-link btn btn-primary" : "nav-link btn"} href="#" id="review-cards" onClick={this.props.setView}>Review Cards</a>
               </li>
-              <li className="nav-item nav-item-custom">
-                <a className={this.props.active === 'create-card' ? "nav-link nav-link-custom" : "nav-link"} href="#" id="create-card" onClick={this.props.setView}>Create New Card</a>
+              <li className="nav-item ml-2 mr-2">
+                <a className={this.props.active === 'create-card' ? "nav-link btn btn-primary" : "nav-link btn"} href="#" id="create-card" onClick={this.props.setView}>Create New Card</a>
               </li>
             </ul>
           </div>
